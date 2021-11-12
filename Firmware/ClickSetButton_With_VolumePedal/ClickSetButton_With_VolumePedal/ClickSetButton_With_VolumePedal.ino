5e37721fcd51d3d790c5ac4cb8236f2cf714f32d
so2akt <17232984+so2akt@users.noreply.github.com>
2021-11-12 20:05:48 +0900

    add: mouse absolute move, relative move, and click in order to click set button on Yukarinatte Connector's voice recognition languages window

--- Firmware/ClickSetButton_With_VolumePedal/ClickSetButton_With_VolumePedal/ClickSetButton_With_VolumePedal.ino
+++ Firmware/ClickSetButton_With_VolumePedal/ClickSetButton_With_VolumePedal/ClickSetButton_With_VolumePedal.ino
@@ -19,6 +19,7 @@ const int pinLed = LED_BUILTIN;
 const int pinButtonClick = 2;
 const int pinButtonMove = 3;
 const int pinButtonScroll = 4;
+bool isExe = false;
 
 void setup() {
   // Prepare led + buttons
@@ -26,15 +27,49 @@ void setup() {
   pinMode(pinButtonClick, INPUT_PULLUP);
   pinMode(pinButtonMove, INPUT_PULLUP);
   pinMode(pinButtonScroll, INPUT_PULLUP);
-
+  
+  // Sends a clean report to the host. This is important on any Arduino type.
+  AbsoluteMouse.begin();
+  
   // Sends a clean report to the host. This is important on any Arduino type.
   Mouse.begin();
 }
 
 void loop() {
+  for(int i=0;i<2 && !isExe;i++)
+  {
+    AbsoluteMouse.moveTo(SHRT_MAX-1920, 1760);
+    for(int i=0;i<5;i++)
+    {
+      Mouse.move(127, 0);
+    }
+    Mouse.click();
+    delay(5000);
+    AbsoluteMouse.moveTo(SHRT_MAX-1920, 1760+720);
+    for(int i=0;i<5;i++)
+    {
+      Mouse.move(127, 0);
+    }
+    Mouse.click();
+    delay(5000);
+    AbsoluteMouse.moveTo(SHRT_MAX-1920, 1760+720+720);
+    for(int i=0;i<5;i++)
+    {
+      Mouse.move(127, 0);
+    }
+    Mouse.click();
+    delay(5000);
+  }
+  isExe = true;
   if (!digitalRead(pinButtonClick)) {
     digitalWrite(pinLed, HIGH);
-
+    AbsoluteMouse.moveTo(32767-1920, 1760);
+    for(int i=0;i<5;i++)
+    {
+      Mouse.move(127, 0);
+    }
+    Mouse.click();
+#if 0
     // Same use as the official library, pretty much self explaining
     Mouse.click();
     //Mouse.click(MOUSE_RIGHT);
@@ -42,27 +77,43 @@ void loop() {
     // Simple debounce
     delay(300);
     digitalWrite(pinLed, LOW);
+#endif
   }
 
   if (!digitalRead(pinButtonMove)) {
     digitalWrite(pinLed, HIGH);
-
+    AbsoluteMouse.moveTo(32767-1920, 1760+720);
+    for(int i=0;i<5;i++)
+    {
+      Mouse.move(127, 0);
+    }
+    Mouse.click();
+#if 0
     // Same use as the official library, pretty much self explaining
-    Mouse.move(100, 0);
+    for(int i=0;i<8;i++)
+      Mouse.move(127, 0);
 
     // Simple debounce
     delay(300);
     digitalWrite(pinLed, LOW);
+#endif
   }
 
   if (!digitalRead(pinButtonScroll)) {
     digitalWrite(pinLed, HIGH);
-
+    AbsoluteMouse.moveTo(32767-1920, 1760+720+720);
+    for(int i=0;i<5;i++)
+    {
+      Mouse.move(127, 0);
+    }
+    Mouse.click();
+#if 0
     // Scroll down a bit, make sure the value is high enough
     Mouse.move(0, 0, 160);
 
     // simple debounce
     delay(300);
     digitalWrite(pinLed, LOW);
+#endif
   }
 }
